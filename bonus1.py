aab5a43cca63a8fc87d077e111a702a91816df89
brendakopp <brenda.kopp.bk@gmail.com>
2024-07-17 02:27:25 -0300

    agrego comentarios

--- bonus1.py
+++ bonus1.py
@@ -1,14 +1,21 @@
 #Inicializamos la matriz edades
+#edades - ARRAY
 edades = [0]*6
 edades = [21, 18, 20, 19, 23, 24]
 
 #Función para mostrar los elementos de un arreglo
+#VARIABLES - TIPO DE DATOS:
+#s - INTEGER
+#z - ARRAY
 def mostrar(z):
     s = len(z)
     for i in range(s):
         print(z[i])
 
-#Función para ordenar los elementos de un arreglo
+#Función para ordenar los elementos de un arreglo con el método de la burbuja
+#VARIABLES - TIPO DE DATOS:
+#s, aux - INTEGER
+#z - ARRAY
 def ordenar(z):
     s = len(z)
     for i in range(s-1):
@@ -19,6 +26,9 @@ def ordenar(z):
                 z[j] = aux
 
 #Función para contar la cantidad de "huecos" que hay en la secuencia
+#VARIABLES - TIPO DE DATOS:
+#s, h - INTEGER
+#z - ARRAY
 def contarhuecos(z):
     s = len(z)
     h = 0
@@ -28,6 +38,16 @@ def contarhuecos(z):
     return h
 
 #Función que crea un arreglo con los elementos que faltan para completar la secuencia
+#VARIABLES - TIPO DE DATOS:
+#s - INTEGER
+#z, g - ARRAY
+
+#COMENTARIO PARA NOSOTROS, DESPUÉS BORRAR: JUSTO EN ESTE EJEMPLO HAY SÓLO 1 HUECO CON UN ELEMENTO FALTANTE, PERO QUISE
+#GENERALIZARLO POR SI LLEGASE A HABER MÁS DE UN HUECO CON ELEMENTOS FALTANTES. TUVE QUE HACER FUNCIONES POR SEPARADAS
+#PORQUE NO SÉ SI TENEMOS PERMITIDO QUE LA FUNCIÓN DEVUELVA MÁS DE UN VALOR, MÁS SI SON TIPOS DE DATOS DIFERENTES, COMO
+#LOS HUECOS UN DATO SIMPLE O LOS ELEMENTOS FALTANTES EN UN ARREGLO. COMO ES UN BONUS, YO NO LE DARÍA MUCHA VUELTA. LO
+#PROBÉ Y FUNCIONAR, FUNCIONA
+
 def faltantes(z):
     s = len(z)
     g = ['']*(s-1)
@@ -39,6 +59,8 @@ def faltantes(z):
     return g
 
 #Función para mostrar los elementos que faltan en la secuencia
+#s - INTEGER
+#z - ARRAY
 def mostrarfaltantes(z):
     s = len(z)
     for i in range(s):
@@ -52,9 +74,11 @@ ordenar(edades)
 print("Reporte de edades ordenadas de forma creciente:")
 mostrar(edades)
 
+#huecos - INTEGER
 huecos = contarhuecos(edades)
 print("Cantidad de huecos en la secuencia: ",huecos)
 
+#edadesFaltantes - ARRAY
 edadesFaltantes = faltantes(edades)
 
 print("El/los elemento/s faltante/s para tener una secuencia autoincremental es/son:")
